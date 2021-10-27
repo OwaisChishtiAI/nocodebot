8e8ebef3fd50812668f088bf635b52f7bbc6fe30
SOCCoreDev <89928541+SOCCoreDev@users.noreply.github.com>
2021-10-27 13:42:09 +0500

    added add button dropdown

---
+++ NCC/assets/scripts/script.js
@@ -0,0 +1,21 @@
+function add_block(){
+    console.log("HIT");
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.fixedButton')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
